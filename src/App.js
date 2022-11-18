8115a2b7c68b32321380d3783c2199693fa17e75
Khalaim Aleksandr <Khalaimaleksandr@gmail.com>
2022-11-19 00:11:35 +0300

    remove files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import MyComponent from './components/MyComponent/MyComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <MyComponent />
+      jshalhlg
     </div>
   );
 }
