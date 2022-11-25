8089229d7debdc3a9a2dc55a9317fdf13a72259e
Khalaim Aleksandr <Khalaimaleksandr@gmail.com>
2022-11-25 18:58:10 +0300

    change browserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import './components/style/_wrapper.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename='4p22-final-project-aleksandr-khalaim'>
+    <HashRouter basename='4p22-final-project-aleksandr-khalaim'>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 );
